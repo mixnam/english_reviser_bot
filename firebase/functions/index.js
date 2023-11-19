9b37440ad8d97f475fb6b128584c7f131b69ce30
Misha <mixnam0905@gmail.com>
2023-11-19 00:25:13 +0000

    Updated to gen2 function

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -7,17 +7,29 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+const {onRequest}= require('firebase-functions/v2/https');
+const {setGlobalOptions}= require('firebase-functions/v2');
+
 const functions = require('firebase-functions');
 // const logger = require("firebase-functions/logger");
 const {Bot} = require('server/src/telegram');
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+setGlobalOptions({
+    region: 'europe-west1'
+})
 
 exports.telegram = functions
     .region('europe-west1')
     .https
     .onRequest((req, res) => {
+    if (req.method === 'POST' && req.url === '/') {
+        const bot = new Bot();
+        bot.handleRequest(req.body);
+    }
+    res.sendStatus(200);
+})
+
+exports.telegram2 = onRequest((req, res) => {
     if (req.method === 'POST' && req.url === '/') {
         const bot = new Bot();
         bot.handleRequest(req.body);
