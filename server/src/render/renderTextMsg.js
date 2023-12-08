724e1b000cb02a98b52b06b968472db396748709
Misha <mixnam0905@gmail.com>
2023-12-08 17:00:41 +0000

    added PT translactions

--- server/src/render/renderTextMsg.js
+++ server/src/render/renderTextMsg.js
@@ -1,118 +1,75 @@
-const {
-  renderWordWithCustomStatus,
-  mapWordProgressToStatus,
-} = require('./renderWord');
+const {renderWordWithCustomStatus, mapWordProgressToStatus} = require('./renderWord');
 const {escapeMarkdown} = require('telegram-escape');
 
 /**
- * @returns string
+ * @typedef {Object} LanguageTranslations
+ * @property {() => string} renderNoMoreWordsToLearnForToday - Translation for 'No more words to learn for today'.
+ * @property {(wordCount: number) => string} renderYouHaveCovered_N_Words - Translation for 'You have covered N words'.
+ * @property {(wordCount: number) => string} renderYouHaveGoneThrough_N_Words - Translation for 'You have gone through N words'.
+ * @property {() => string} renderNoMoreWordsToReviseForToday - Translation for 'No more words to revise for today'.
+ * @property {(wordCount: number) => string} renderYouHaveRevised_N_Words - Translation for 'You have revised N words'.
+ * @property {() => string} renderNoIdea - Translation for 'Have no idea what you want from me'.
+ * @property {() => string} renderSendMeContextForThisWord - Translation for 'Send me a context (e.g., meaning, usage, etc.) for this word'.
+ * @property {() => string} renderSendMePictureForThisWord - Translation for 'Send me a picture for this word'.
+ * @property {() => string} renderSendMeWordToAdd - Translation for 'Send me a word you want to add'.
+ * @property {() => string} renderSendMeTranslationForThisWord - Translation for 'Send me translation for this word'.
+ * @property {() => string} renderYouAreNotMyMaster - Translation for 'You are not my master, I am not your slave'.
+ * @property {(word: import("../repo/words").Word) => string} renderYouHaveMovedThisWordBackToStady - Translation for 'You have moved this word back to the learning list'.
+ * @property {() => string} renderDoYouWantToAddContext - Translation for 'Do you want to add some context (e.g., meaning, usage, etc.) to this word?'.
+ * @property {() => string} renderDoYouWantToAddPicture - Translation for 'Do you want to add any picture to this word?'.
+ * @property {(word: string) => string} renderYouAreAddingExistingWord - Translation for 'You are adding the following word'.
+ * @property {(word: import("../repo/words").Word) => string} renderYouJustAddedNewWord - Translation for 'You just added new word'.
  */
-const renderNoMoreWordsToLearnForToday = () => `You've covered all the words designated for learning today 🎉
-Come back and repeat tomorrow!`;
 
 /**
- * @param {number} wordCount
- * @returns strng
+ * @type {Object.<string, LanguageTranslations>}
  */
-const renderYouHaveCovered_N_Words = (wordCount) => `You've covered ${wordCount} words today`;
-
-/**
- * @param {number} wordCount
- * @returns string
- */
-const renderYouHaveGoneThrough_N_Words = (wordCount) => `You've gone through ${wordCount} words! Great result 🎉`;
-
-/**
- * @returns string
- */
-const renderNoMoreWordsToReviseForToday = () => `You've revised all your words for today 🎉
-Come back and repeat tomorrow!`;
-
-/**
- * @param {number} wordCount
- * @returns string
- */
-const renderYouHaveRevised_N_Words = (wordCount) => `You've revised ${wordCount} words today`;
-
-/**
- * @returns string
- */
-const renderNoIdea = () => 'Have no idea what you want from me';
-
-/**
- * @returns string
- */
-const renderSendMeContextForThisWord = () => escapeMarkdown('Send me a context (e.g. meaning, usage, etc.) for this word');
-
-/**
- * @returns string
- */
-const renderSendMePictureForThisWord = () => 'Send me a picture for this word';
-
-/**
- * @returns string
- */
-const renderSendMeWordToAdd = () => 'Send me a word you want to add';
-
-/**
- * @returns string
- */
-const renderSendMeTranslationForThisWord = () => 'Send me translation for this word';
-
-/**
- * @returns string
- */
-const renderYouAreNotMyMaster = () => 'You are not my master, I am not your slave';
-
-/**
- * @param {import("../repo/words").Word} word
- * @returns
- */
-const renderYouHaveMovedThisWordBackToStady = (word) => `You've moved this word back to the learning list
-${renderWordWithCustomStatus(word, mapWordProgressToStatus[word.Progress])}`;
-
-/**
- * @returns string
- */
-const renderDoYouWantToAddContext = () => escapeMarkdown('Do you want to add some context (e.g. meaning, usage, etc.) to this word?');
-
-/**
- * @returns string
- */
-const renderDoYouWantToAddPicture = () => 'Do you want to add any picture to this word?';
-
-/**
- * @param {string} word
- * @returns string
- */
-const renderYouAreAddingExistingWord = (word) => `You're adding the following word \\- __${word}__
-
-There are already some similar words in your word list\\. Did you forget about them? 
-Click on the correct word to move it to the 'Have Problems' state, or click 'Continue' if you are adding new word`;
-
-/**
- * @param {Pick<import('../repo/words').Word, 'English' | 'Translation' | 'Examples'>} word
- * @returns string
- */
-const renderYouJustAddedNewWord = (word) => `You just added new word 🎉: 
-${renderWordWithCustomStatus(word)}`;
-
-module.exports = {
-  renderDoYouWantToAddContext,
-  renderNoIdea,
-  renderNoMoreWordsToLearnForToday,
-  renderNoMoreWordsToReviseForToday,
-  renderSendMeContextForThisWord,
-  renderSendMeTranslationForThisWord,
-  renderSendMeWordToAdd,
-  renderYouAreAddingExistingWord,
-  renderYouHaveCovered_N_Words,
-  renderYouHaveGoneThrough_N_Words,
-  renderYouHaveMovedThisWordBackToStady,
-  renderYouAreNotMyMaster,
-  renderYouHaveRevised_N_Words,
-  renderYouJustAddedNewWord,
-  renderDoYouWantToAddPicture,
-  renderSendMePictureForThisWord,
+const languageTokenMap = {
+  en: {
+    renderNoMoreWordsToLearnForToday: () => `You've covered all the words designated for learning today 🎉
+Come back and repeat tomorrow!`,
+    renderYouHaveCovered_N_Words: (wordCount) => `You've covered ${wordCount} words today`,
+    renderYouHaveGoneThrough_N_Words: (wordCount) => `You've gone through ${wordCount} words! Great result 🎉`,
+    renderNoMoreWordsToReviseForToday: () => `You've revised all your words for today 🎉
+Come back and repeat tomorrow!`,
+    renderYouHaveRevised_N_Words: (wordCount) => `You've revised ${wordCount} words today`,
+    renderNoIdea: () => 'Have no idea what you want from me',
+    renderSendMeContextForThisWord: () => escapeMarkdown('Send me a context (e.g. meaning, usage, etc.) for this word'),
+    renderSendMePictureForThisWord: () => 'Send me a picture for this word',
+    renderSendMeWordToAdd: () => 'Send me a word you want to add',
+    renderSendMeTranslationForThisWord: () => 'Send me translation for this word',
+    renderYouAreNotMyMaster: () => 'You are not my master, I am not your slave',
+    renderYouHaveMovedThisWordBackToStady: (word) => `You've moved this word back to the learning list\n${renderWordWithCustomStatus(word, mapWordProgressToStatus[word.Progress])}`,
+    renderDoYouWantToAddContext: () => escapeMarkdown('Do you want to add some context (e.g. meaning, usage, etc.) to this word?'),
+    renderDoYouWantToAddPicture: () => 'Do you want to add any picture to this word?',
+    renderYouAreAddingExistingWord: (word) => `You're adding the following word \\- __${word}__\nThere are already some similar words in your word list\\. Did you forget about them? \nClick on the correct word to move it to the 'Have Problems' state, or click 'Continue' if you are adding new word`,
+    renderYouJustAddedNewWord: (word) => `You just added new word 🎉: \n${renderWordWithCustomStatus(word)}`,
+  },
+  pt: {
+    renderNoMoreWordsToLearnForToday: () => `Cobriu todas as palavras designadas para aprendizado hoje 🎉
+Volte e repita amanhã!`,
+    renderYouHaveCovered_N_Words: (wordCount) => `Cobriu ${wordCount} palavras hoje`,
+    renderYouHaveGoneThrough_N_Words: (wordCount) => `Passou por ${wordCount} palavras! Ótimo resultado 🎉`,
+    renderNoMoreWordsToReviseForToday: () => `Revisou todas as suas palavras hoje 🎉
+Volte e repita amanhã!`,
+    renderYouHaveRevised_N_Words: (wordCount) => `Revisou ${wordCount} palavras hoje`,
+    renderNoIdea: () => 'Não faço ideia do que quer de mim',
+    renderSendMeContextForThisWord: () => escapeMarkdown('Envie-me um contexto (por exemplo, significado, uso, etc.) para esta palavra'),
+    renderSendMePictureForThisWord: () => 'Envie-me uma imagem para esta palavra',
+    renderSendMeWordToAdd: () => 'Envie-me uma palavra que deseja adicionar',
+    renderSendMeTranslationForThisWord: () => 'Envie-me a tradução para esta palavra',
+    renderYouAreNotMyMaster: () => 'Não é meu mestre, não sou seu escravo',
+    renderYouHaveMovedThisWordBackToStady: (word) => `Moveu esta palavra de volta para a lista de aprendizado\n${renderWordWithCustomStatus(word, mapWordProgressToStatus[word.Progress])}`,
+    renderDoYouWantToAddContext: () => escapeMarkdown('Quer adicionar algum contexto (por exemplo, significado, uso, etc.) a esta palavra?'),
+    renderDoYouWantToAddPicture: () => 'Quer adicionar alguma imagem a esta palavra?',
+    renderYouAreAddingExistingWord: (word) => `Está adicionando a seguinte palavra \\- __${word}__\nJá existem palavras semelhantes em sua lista de palavras\\. Esqueceu delas? \nClique na palavra correta para movê-la para o estado 'Tem Problemas', ou clique em 'Continuar' se estiver adicionando uma nova palavra`,
+    renderYouJustAddedNewWord: (word) => `Acabou de adicionar uma nova palavra 🎉: \n${renderWordWithCustomStatus(word)}`,
+  },
 };
 
+/**
+ * @type {LanguageTranslations}
+ */
+const translations = languageTokenMap[process.env.LANGUAGE.startsWith('pt') ? 'pt' : 'en'];
+
+module.exports = translations;
--- server/src/render/renderWord.js
+++ server/src/render/renderWord.js
@@ -1,39 +1,87 @@
 const {escapeMarkdown} = require('telegram-escape');
 const {Progress} = require('../repo/words');
 
+/**
+ * @typedef {Object} LanguageTranslations
+ * @property {string} learned - Translation for 'Learned'.
+ * @property {string} activeLearning - Translation for 'Active Learning'.
+ * @property {string} needToRepeat - Translation for 'Need to repeat'.
+ * @property {string} haveToPayAttention - Translation for 'Have to pay attention'.
+ * @property {string} haveProblems - Translation for 'Have problems'.
+ * @property {string} englishLabel - Translation for the label 'English'.
+ * @property {string} examplesLabel - Translation for the label 'Examples'.
+ * @property {string} translationLabel - Translation for the label 'Translation'.
+ */
+
+/**
+ * @type {Object.<string, LanguageTranslations>}
+ */
+const languageTokenMap = {
+  en: {
+    learned: '*Learned *🟢',
+    activeLearning: '*Active Learning 🔵*',
+    needToRepeat: '*Need to repeat 🟡*',
+    haveToPayAttention: '*Have to pay attention 🟠*',
+    haveProblems: '*Have problems 🔴*',
+    englishLabel: 'English',
+    examplesLabel: 'Examples',
+    translationLabel: 'Translation',
+  },
+  pt: {
+    learned: '*Aprendido 🟢*',
+    activeLearning: '*Aprendizado Ativo 🔵*',
+    needToRepeat: '*Necessita Repetição 🟡*',
+    haveToPayAttention: '*Precisa Prestar Atenção 🟠*',
+    haveProblems: '*Apresenta Problemas 🔴*',
+    englishLabel: 'Inglês',
+    examplesLabel: 'Exemplos',
+    translationLabel: 'Tradução',
+  },
+};
+
+const languageToken = process.env.LANGUAGE.startsWith('pt') ? 'pt' : 'en';
+
+const {
+  learned,
+  activeLearning,
+  needToRepeat,
+  haveToPayAttention,
+  haveProblems,
+} = languageTokenMap[languageToken];
+
 const mapWordProgressToStatus = {
-  [Progress.Learned]: '*Learned 🟢*',
-  [Progress.ActiveLearning]: '*Active Learning 🔵*',
-  [Progress.NeedToRepeat]: '*Need to repeat 🟡*',
-  [Progress.HaveToPayAttention]: '*Have to pay attention 🟠*',
-  [Progress.HaveProblems]: '*Have problems 🔴*',
+  [Progress.Learned]: learned,
+  [Progress.ActiveLearning]: activeLearning,
+  [Progress.NeedToRepeat]: needToRepeat,
+  [Progress.HaveToPayAttention]: haveToPayAttention,
+  [Progress.HaveProblems]: haveProblems,
 };
 
 // eslint-disable-next-line
 /**
-   * @param {Pick<import('../repo/words').Word, 'English' | 'Translation' | 'Examples'>} word
-   * @param {string} [status]
-   * @return {string}
-   */
+ * @param {Pick<import('../repo/words').Word, 'English' | 'Translation' | 'Examples'>} word
+ * @param {string} [status]
+ * @return {string}
+ */
 const renderWordWithCustomStatus = (word, status) => {
   const english = escapeMarkdown(word.English);
   const translation = escapeMarkdown(word.Translation);
   const examples = word.Examples ? escapeMarkdown(word.Examples) : null;
 
   return `
-*English:*
+*${languageTokenMap[languageToken].englishLabel}:*
 ${english} ${status ? `\\- ${status}` : ''}
 ${examples ? `
-*Examples:*
+*${languageTokenMap[languageToken].examplesLabel}:*
 ${examples}
 `: ''}
-*Translation:*
+*${languageTokenMap[languageToken].translationLabel}:*
 ||${translation}||
-          `;
+  `;
 };
-
 
 module.exports = {
   renderWordWithCustomStatus,
   mapWordProgressToStatus,
 };
+
