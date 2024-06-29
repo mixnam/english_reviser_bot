cec99344431b635ec8ac5b90c69d90e3fc76608a
Misha <mixnam0905@gmail.com>
2024-06-29 19:54:41 +0100

    [edit-word-hack]: added message_id to WebApp search params

--- server/src/commands/learn.js
+++ server/src/commands/learn.js
@@ -77,64 +77,25 @@ class LearnCommand extends Command {
      */
     const options = {
       parse_mode: 'MarkdownV2',
-      reply_markup: {
-        inline_keyboard: [
-          [{
-            text: labelUp,
-            callback_data: [
-              LearnCallbackId,
-              word._id,
-              'true',
-              wordCount ?? 0,
-            ].join(','),
-          },
-          {
-            text: labelDown,
-            callback_data: [
-              LearnCallbackId,
-              word._id,
-              'false',
-              wordCount ?? 0,
-            ].join(','),
-          }],
-          [
-            {
-              text: 'Edit word',
-              web_app: {
-                url: process.env.TMA_URL + '#/edit-word?word=' + btoa(
-                    encodeURIComponent(JSON.stringify(wordToEdit)),
-                ) + '&chat_id=' + msg.chat.id,
-              },
-            },
-            {
-              text: 'Refresh',
-              callback_data: [
-                LearnCallbackId,
-                'refresh',
-                word._id,
-              ].join(','),
-            },
-          ],
-          [
-            {
-              text: labelStopLearning,
-              callback_data: [
-                LearnCallbackId,
-                wordCount ?? 0,
-              ].join(','),
-            },
-          ],
-        ],
-      },
     };
 
     if (word.TelegramPictureID) {
       await this.#bot.sendPhoto(msg.chat.id, word.TelegramPictureID);
     }
 
+    if (!(Boolean(word.Audio) || Boolean(word.TelegramAudioID))) {
+      // if there is no audio at all, just send text msg and that's it
+      await this.#bot.sendMessage(
+          msg.chat.id,
+          text,
+          options,
+      );
+      return;
+    }
+
     /**
-       * @type {import('node-telegram-bot-api').Message}
-       */
+     * @type {import('node-telegram-bot-api').Message}
+     */
     let sentMsg;
     if (word.TelegramAudioID) {
       sentMsg = await this.#bot.sendVoice(
@@ -143,10 +104,7 @@ class LearnCommand extends Command {
             ...options,
             caption: text,
           });
-      return;
-    }
-
-    if (word.Audio) {
+    } else if (word.Audio) {
       sentMsg = await this.#bot.sendVoice(
           msg.chat.id, Buffer.from(word.Audio),
           {
@@ -157,13 +115,56 @@ class LearnCommand extends Command {
         setWordTelegramAudioID(word._id, sentMsg.voice.file_id, this.logger.child(ctx))
             .catch((err) => this.logger.error(ctx, err));
       }
-      return;
     }
 
-    this.#bot.sendMessage(
-        msg.chat.id,
-        text,
-        options,
+    this.#bot.editMessageReplyMarkup({
+      inline_keyboard: [
+        [{
+          text: labelUp,
+          callback_data: [
+            LearnCallbackId,
+            word._id,
+            'true',
+            wordCount ?? 0,
+          ].join(','),
+        },
+        {
+          text: labelDown,
+          callback_data: [
+            LearnCallbackId,
+            word._id,
+            'false',
+            wordCount ?? 0,
+          ].join(','),
+        }],
+        [
+          {
+            text: 'Edit word',
+            web_app: {
+              url: process.env.TMA_URL +
+                '#/edit-word?word=' + btoa(
+                  encodeURIComponent(JSON.stringify(wordToEdit)),
+              ) +
+                '&chat_id=' + msg.chat.id +
+                '&message_id=' + sentMsg.message_id,
+            },
+          },
+        ],
+        [
+          {
+            text: labelStopLearning,
+            callback_data: [
+              LearnCallbackId,
+              wordCount ?? 0,
+            ].join(','),
+          },
+        ],
+      ],
+    },
+    {
+      chat_id: ctx.chatID,
+      message_id: sentMsg.message_id,
+    },
     );
   };
 
@@ -184,116 +185,6 @@ class LearnCommand extends Command {
           msg.chat.id,
           renderYouHaveCovered_N_Words(data.wordCount),
       );
-      return;
-    }
-
-    if (data.type === 'refresh') {
-      const word = await getWordByID(data.wordID, this.logger.child(ctx));
-      if (word instanceof Error) {
-        this.logger.error(ctx, word);
-        return;
-      }
-
-      /**
-     * @type {Partial<import('../repo/words').Word>}
-     */
-      const wordToEdit = {
-        _id: word._id,
-        English: word.English,
-        Examples: word.Examples,
-        Translation: word.Translation,
-      };
-
-      const currentProgressIdx = ProgressOrder.findIndex(
-          (i) => i === word.Progress,
-      );
-
-      /**
-       * @type {TelegramBot.EditMessageTextOptions}
-       */
-      const msgOptions = {
-        parse_mode: 'MarkdownV2',
-        message_id: msg.message_id,
-        chat_id: msg.chat.id,
-        reply_markup: msg.reply_markup,
-      };
-
-      const wordText = renderWordWithCustomStatus(
-          word,
-          mapWordProgressToStatus[ProgressOrder[currentProgressIdx]],
-      );
-
-      if (msg.text && !word.Audio) {
-        this.#bot.editMessageText(wordText, msgOptions);
-      } else if (msg.caption) {
-        /**
-         * @type {TelegramBot.SendMessageOptions}
-         */
-        const options = {
-          parse_mode: 'MarkdownV2',
-          reply_markup: {
-            inline_keyboard: [
-              [{
-                text: labelUp,
-                callback_data: [
-                  LearnCallbackId,
-                  word._id,
-                  'true',
-                  0,
-                ].join(','),
-              },
-              {
-                text: labelDown,
-                callback_data: [
-                  LearnCallbackId,
-                  word._id,
-                  'false',
-                  0,
-                ].join(','),
-              }],
-              [
-                {
-                  text: 'Edit word',
-                  web_app: {
-                    url: process.env.TMA_URL + '#/edit-word?word=' + btoa(
-                        encodeURIComponent(JSON.stringify(wordToEdit)),
-                    ) + '&chat_id=' + msg.chat.id,
-                  },
-                },
-                {
-                  text: 'Refresh',
-                  callback_data: [
-                    LearnCallbackId,
-                    'refresh',
-                    word._id,
-                  ].join(','),
-                },
-              ],
-              [
-                {
-                  text: labelStopLearning,
-                  callback_data: [
-                    LearnCallbackId,
-                    0,
-                  ].join(','),
-                },
-              ],
-            ],
-          },
-        };
-
-        await this.#bot.deleteMessage(msg.chat.id, msg.message_id);
-        const sentMsg = await this.#bot.sendVoice(
-            msg.chat.id, Buffer.from(word.Audio),
-            {
-              ...options,
-              caption: wordText,
-            });
-        if (sentMsg.voice) {
-          setWordTelegramAudioID(word._id, sentMsg.voice.file_id, this.logger.child(ctx))
-              .catch((err) => this.logger.error(ctx, err));
-        }
-      }
       return;
     }
 
@@ -353,7 +244,7 @@ class LearnCommand extends Command {
 
   /**
    * @typedef CallbackData
-   * @type {StopCallbackData|LearnCallbackData|RefreshCallback}
+   * @type {StopCallbackData|LearnCallbackData}
    */
 
   /**
@@ -361,13 +252,6 @@ class LearnCommand extends Command {
    * @type {object}
    * @property {'stop'} type
    * @property {number} wordCount
-   */
-
-  /**
-   * @typedef RefreshCallback
-   * @type {object}
-   * @property {'refresh'} type
-   * @property {string} wordID
    */
 
   /**
@@ -395,12 +279,6 @@ class LearnCommand extends Command {
         wordID: rawData[0],
         remember: rawData[1] === 'true',
         wordCount: Number.parseInt(rawData[2]),
-      };
-    }
-    if (rawData.length === 2 && rawData[0] === 'refresh') {
-      return {
-        type: 'refresh',
-        wordID: rawData[1],
       };
     }
     if (rawData.length === 1 && typeof rawData[0] === 'string') {
