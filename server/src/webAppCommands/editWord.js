e1ead991ce18f4b5a27ef8661352b0ab94337c2a
Misha <mixnam0905@gmail.com>
2024-06-30 13:49:21 +0100

    [edit-word-hack]: injectd Bot into Api, port whole telegram logic into Bot

---
+++ server/src/webAppCommands/editWord.js
@@ -0,0 +1,78 @@
+const {updateWord} = require('../repo/words');
+
+const {TTSService} = require('../tts/tts');
+
+const {WebAppCommand} = require('./webAppCommand');
+
+/**
+ * @typedef EditWordPayload
+ * @type {object}
+ * @property {number} chatID
+ * @property {number} messageID
+ * @property {import('../repo/words').Word} word
+ */
+
+/**
+ * @typedef EditWordMsg
+ * @type {object}
+ * @property {'edit_word_msg'} type
+ * @property {EditWordPayload} payload
+ */
+
+/**
+ * @extends {WebAppCommand<EditWordMsg>}
+ */
+class EditWordCommand extends WebAppCommand {
+  /**
+   * @param {import('node-telegram-bot-api')} bot
+   * @param {import('./webAppCommand').Logger} logger
+   * @param {(chatID: number, word: import('../repo/words').Word, wordCount?: number) => Promise<void>} sendWord
+   */
+  constructor(bot, logger, sendWord) {
+    super(bot, logger);
+    this.sendWord = sendWord;
+  }
+
+  /**
+   * @param {EditWordMsg} msg
+   */
+  async processMsg(msg) {
+    const {
+      chatID,
+      messageID,
+      word,
+    } = msg.payload;
+    const user = await this.getSessionUser(chatID);
+    if (user instanceof Error) {
+      this.logger.error(user);
+      return user;
+    }
+
+    const audio = await TTSService.getAudioForText(word.English);
+
+    if (audio instanceof Error) {
+      this.logger.error(audio);
+      return audio;
+    } else {
+      word.Audio = audio;
+    }
+
+    const result = await updateWord(
+        user._id,
+        word,
+        this.logger,
+    );
+
+    if (result instanceof Error) {
+      this.logger.error(result);
+      return result;
+    }
+
+    await this.bot.deleteMessage(chatID, messageID);
+    await this.sendWord(chatID, word);
+  }
+}
+
+module.exports = {
+  EditWordCommand,
+};
---
+++ server/src/webAppCommands/webAppCommand.js
@@ -0,0 +1,72 @@
+const {getUserByChatID} = require('../repo/users');
+
+/**
+ * @typedef LogFn
+ * @type {{
+ *  (ctx: Object, msg: string) : void;
+ *  (ctx: Object, error: Error) : void;
+ *  (msg: string) : void
+ *  (error: Error) : void
+ * }}
+ */
+
+/**
+ * @typedef Logger
+ * @type {Object}
+ * @property {LogFn} info
+ * @property {LogFn} debug
+ * @property {LogFn} error
+ * @property {(props: Object) => Logger} child
+ */
+
+
+/**
+ * Basic interface for bot web app command
+ *
+ * @template M
+ * @abstract
+ */
+class WebAppCommand {
+  /**
+   * @type {Logger}
+   */
+  logger;
+  bot;
+
+  /**
+   * @param {import('node-telegram-bot-api')} bot
+   * @param {Logger} logger
+   */
+  constructor(bot, logger) {
+    this.bot = bot;
+    this.logger = logger;
+  }
+
+
+  /**
+   * @param {number} chatID
+   *
+   * @return {Promise<import('../repo/users').User|Error>}
+   */
+  getSessionUser = async (chatID) => {
+    const ctx = {chatID: chatID};
+    const user = await getUserByChatID(chatID, this.logger.child(ctx));
+    if (user instanceof Error) {
+      return user;
+    }
+    return user;
+  };
+
+  /**
+   * @param {M} msg
+   *
+   * @returns {Promise<Error|null>}
+   */
+  async processMsg(msg) {
+    throw new Error(`processMsg method is not implemented - ${msg}`);
+  };
+}
+
+module.exports = {
+  WebAppCommand,
+};
